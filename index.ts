2fcec123523baf9aaa2979ae8605fd3ace415b42
kaStinju <40683050+kaStinju@users.noreply.github.com>
2021-11-14 21:45:49 -0500

    first photo branch edit

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import * as dotenv from "dotenv";
 async function run() {
   dotenv.config();
 
-  const ticker = "LINK";
+  const ticker = "LRC";
 
   const currency = "USD";
   const from = "2021-11-06";
@@ -27,16 +27,47 @@ async function run() {
   const prices = res.data.data.prices.map(({ price }) => price);
   console.log(prices);
 
-  drawPicture();
+  drawPicture(prices);
 }
 
-async function drawPicture() {
+async function drawPicture(arr) {
+  const boxX = 376;
+  const boxY = 60;
+  const boxWidth = 336;
+  const boxHeight = 198;
+
+  const graphX = boxX + 20;
+  const graphY = 60;
+  const graphWidth = boxWidth - 40;
+  const graphHeight = 198;
+
+  const largest = Math.max(...arr);
+  const smallest = Math.min(...arr) * 0.995;
+  const arr2 = arr
+    .map((x) => x - smallest)
+    .map((x) => x / (largest - smallest));
+  const numberOfBars = arr.length;
+  const barWidth = graphWidth / numberOfBars;
+
+  console.log(arr2);
   const overlay = await loadImage("./test.png");
   const canvas = createCanvas(790, 460);
   const ctx = canvas.getContext("2d");
-  ctx.rect(376, 65, 710, 257);
+  ctx.rect(graphX, graphY, graphWidth, graphHeight);
   ctx.fillStyle = "blue";
   ctx.fill();
+  ctx.fillStyle = "red";
+  for (let i = 0; i < numberOfBars; i++) {
+    const barX =
+      graphWidth / 2 - barWidth * (numberOfBars / 2) + graphX + barWidth * i;
+    ctx.fillRect(
+      barX,
+      graphY + graphHeight * (1 - arr2[i]),
+      barWidth,
+      graphHeight * arr2[i]
+    );
+    console.log(barX, graphY, barWidth, graphHeight * arr2[i]);
+  }
   ctx.drawImage(overlay, 0, 0, 790, 460);
   const buffer = canvas.toBuffer("image/png");
   fs.writeFileSync("./image.png", buffer);
